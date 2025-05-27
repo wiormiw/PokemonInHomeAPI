fe2e704acd149ede2b9194bd21455418dba218c5
Wijoyo Raharjo Murti Indra Wahyono <wwijoyo2@gmail.com>
2025-05-27 13:02:23 +0700

    feat: PlayerPokemon Entity + Configuration

---
+++ src/Domain/Entities/PlayerPokemon.cs
@@ -0,0 +1,18 @@
+﻿namespace PokemonInHomeAPI.Domain.Entities;
+
+public class PlayerPokemon : BaseAuditableEntity
+{
+    public int PlayerId { get; set; }
+    
+    public int PokemonId { get; set; }
+    
+    public bool IsActive { get; set; }
+
+    public string Nickname { get; set; } = string.Empty;
+    
+    public DateTimeOffset CaughtAt { get; set; } = DateTime.UtcNow;
+    
+    public Player? Player { get; set; }
+    
+    public Pokemon? Pokemon { get; set; }
+}
---
+++ src/Infrastructure/Data/Configurations/PlayerPokemonConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PokemonInHomeAPI.Domain.Entities;
+
+public class PlayerPokemonConfiguration : IEntityTypeConfiguration<PlayerPokemon>
+{
+    public void Configure(EntityTypeBuilder<PlayerPokemon> builder)
+    {
+        builder.HasKey(pp => new { pp.PlayerId, pp.PokemonId });
+
+        builder.Property(pp => pp.IsActive)
+            .IsRequired();
+        
+        builder.Property(pp => pp.Nickname)
+            .HasMaxLength(50);
+
+        builder.Property(pp => pp.CaughtAt)
+            .IsRequired();
+
+        builder.HasOne(pp => pp.Player)
+            .WithMany(p => p.PlayerPokemons)
+            .HasForeignKey(pp => pp.PlayerId);
+
+        builder.HasOne(pp => pp.Pokemon)
+            .WithMany(p => p.PlayerPokemons)
+            .HasForeignKey(pp => pp.PokemonId);
+    }
+}
