c50fb64fc09593b654eafda6ddcbd958d8b61a82
Wijoyo Raharjo Murti Indra Wahyono <wwijoyo2@gmail.com>
2025-05-27 13:02:35 +0700

    feat: PokemonMove Entity + Configuration

---
+++ src/Domain/Entities/PokemonMove.cs
@@ -0,0 +1,16 @@
+﻿namespace PokemonInHomeAPI.Domain.Entities;
+
+public class PokemonMove
+{
+    public int PokemonId { get; set; }
+    
+    public int MoveId { get; set; }
+    
+    public int Slot { get; set; }
+    
+    public int CurrentPp { get; set; }
+
+    public Pokemon? Pokemon { get; set; }
+    
+    public Move? Move { get; set; }
+}
---
+++ src/Infrastructure/Data/Configurations/PokemonMoveConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PokemonInHomeAPI.Domain.Entities;
+
+namespace PokemonInHomeAPI.Infrastructure.Data.Configurations;
+
+public class PokemonMoveConfiguration : IEntityTypeConfiguration<PokemonMove>
+{
+    public void Configure(EntityTypeBuilder<PokemonMove> builder)
+    {
+        builder.HasKey(pm => new { pm.PokemonId, pm.MoveId });
+
+        builder.Property(pm => pm.Slot)
+            .IsRequired();
+
+        builder.Property(pm => pm.CurrentPp)
+            .IsRequired();
+
+        builder.HasOne(pm => pm.Pokemon)
+            .WithMany(p => p.PokemonMoves)
+            .HasForeignKey(pm => pm.PokemonId);
+
+        builder.HasOne(pm => pm.Move)
+            .WithMany(m => m.PokemonMoves)
+            .HasForeignKey(pm => pm.MoveId);
+    }
+}
+
