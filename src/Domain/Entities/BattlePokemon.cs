3a1bc3d72c9e436b265c332fa1bf937497ff526a
Wijoyo Raharjo Murti Indra Wahyono <wwijoyo2@gmail.com>
2025-05-27 13:02:59 +0700

    feat: BattlePokemon Entity + Configuration

---
+++ src/Domain/Entities/BattlePokemon.cs
@@ -0,0 +1,16 @@
+﻿namespace PokemonInHomeAPI.Domain.Entities;
+
+public class BattlePokemon
+{
+    public int BattleId { get; set; }
+    
+    public int PokemonId { get; set; }
+    
+    public int PlayerId { get; set; }
+
+    public Battle? Battle { get; set; }
+    
+    public Pokemon? Pokemon { get; set; }
+    
+    public Player? Player { get; set; }
+}
---
+++ src/Infrastructure/Data/Configurations/BattlePokemonConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PokemonInHomeAPI.Domain.Entities;
+
+namespace PokemonInHomeAPI.Infrastructure.Data.Configurations;
+
+public class BattlePokemonConfiguration : IEntityTypeConfiguration<BattlePokemon>
+{
+    public void Configure(EntityTypeBuilder<BattlePokemon> builder)
+    {
+        builder.HasKey(bp => new { bp.BattleId, bp.PokemonId });
+
+        builder.Property(bp => bp.PlayerId)
+            .IsRequired();
+
+        builder.HasOne(bp => bp.Battle)
+            .WithMany(b => b.BattlePokemons)
+            .HasForeignKey(bp => bp.BattleId);
+
+        builder.HasOne(bp => bp.Pokemon)
+            .WithMany(p => p.BattlePokemons)
+            .HasForeignKey(bp => bp.PokemonId);
+
+        builder.HasOne(bp => bp.Player)
+            .WithMany()
+            .HasForeignKey(bp => bp.PlayerId);
+    }
+}
