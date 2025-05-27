3b917d56065d74da3c6276760756fa99e4e4319a
Wijoyo Raharjo Murti Indra Wahyono <wwijoyo2@gmail.com>
2025-05-27 13:21:12 +0700

    feat: Register Entity in DBContext

--- src/Infrastructure/Data/ApplicationDbContext.cs
+++ src/Infrastructure/Data/ApplicationDbContext.cs
@@ -14,8 +14,20 @@ public class ApplicationDbContext : IdentityDbContext<ApplicationUser>, IApplica
     }
 
     public DbSet<TodoList> TodoLists => Set<TodoList>();
-
     public DbSet<TodoItem> TodoItems => Set<TodoItem>();
+    public DbSet<Player> Players => Set<Player>();
+    public DbSet<PokemonSpecies> PokemonSpecies => Set<PokemonSpecies>();
+    public DbSet<Pokemon> Pokemons => Set<Pokemon>();
+    public DbSet<PlayerPokemon> PlayerPokemons => Set<PlayerPokemon>();
+    public DbSet<Pokedex> Pokedexes => Set<Pokedex>();
+    public DbSet<Move> Moves => Set<Move>();
+    public DbSet<PokemonMove> PokemonMoves => Set<PokemonMove>();
+    public DbSet<Item> Items => Set<Item>();
+    public DbSet<PlayerItem> PlayerItems => Set<PlayerItem>();
+    public DbSet<Battle> Battles => Set<Battle>();
+    public DbSet<BattlePokemon> BattlePokemons => Set<BattlePokemon>();
+    public DbSet<Trade> Trades => Set<Trade>();
+    public DbSet<TradePokemon> TradePokemons => Set<TradePokemon>();
 
     protected override void OnModelCreating(ModelBuilder builder)
     {
