34bc6f1d1d288d384de764262c369deeb21a4e87
Wijoyo Raharjo Murti Indra Wahyono <wwijoyo2@gmail.com>
2025-05-27 12:58:47 +0700

    feat: Trade Entity + Configuration

---
+++ src/Domain/Entities/Trade.cs
@@ -0,0 +1,16 @@
+﻿namespace PokemonInHomeAPI.Domain.Entities;
+
+public class Trade : BaseAuditableEntity
+{
+    public int Player1Id { get; set; }
+    
+    public int Player2Id { get; set; }
+    
+    public DateTimeOffset TradeDate { get; set; } = DateTimeOffset.Now;
+
+    public Player? Player1 { get; set; }
+
+    public Player? Player2 { get; set; }
+    
+    public ICollection<TradePokemon> TradePokemons { get; set; } = new List<TradePokemon>();
+}
---
+++ src/Infrastructure/Data/Configurations/TradeConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PokemonInHomeAPI.Domain.Entities;
+
+namespace PokemonInHomeAPI.Infrastructure.Data.Configurations;
+
+public class TradeConfiguration : IEntityTypeConfiguration<Trade>
+{
+    public void Configure(EntityTypeBuilder<Trade> builder)
+    {
+        builder.HasKey(t => t.Id);
+
+        builder.Property(t => t.TradeDate)
+            .IsRequired();
+
+        builder.HasOne(t => t.Player1)
+            .WithMany(p => p.Player1Trades)
+            .HasForeignKey(t => t.Player1Id)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        builder.HasOne(t => t.Player2)
+            .WithMany(p => p.Player2Trades)
+            .HasForeignKey(t => t.Player2Id)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        builder.HasMany(t => t.TradePokemons)
+            .WithOne(tp => tp.Trade)
+            .HasForeignKey(tp => tp.TradeId);
+    }
+}
