d8e6cc9990c3929911634c38db3c0aeceba10841
Wijoyo Raharjo Murti Indra Wahyono <wwijoyo2@gmail.com>
2025-05-27 13:01:50 +0700

    feat: PokemonSpecies Entity + Configuration

---
+++ src/Domain/Entities/PokemonSpecies.cs
@@ -0,0 +1,25 @@
+﻿namespace PokemonInHomeAPI.Domain.Entities;
+
+public class PokemonSpecies : BaseAuditableEntity
+{
+    public string Name { get; set; } = string.Empty;
+
+    public PokemonType Type1 { get; set; } = new PokemonType("Unknown");
+    
+    public PokemonType? Type2 { get; set; }
+    
+    public int BaseHp { get; set; }
+    
+    public int BaseAttack { get; set; }
+    
+    public int BaseDefense { get; set; }
+    
+    public int BaseSpeed { get; set; }
+    
+    public int BaseSpecialAttack { get; set; }
+    
+    public int BaseSpecialDefense { get; set; }
+    
+    public ICollection<Pokemon> Pokemons { get; set; } = new List<Pokemon>();
+    public ICollection<Pokedex> Pokedexes { get; set; } = new List<Pokedex>();
+}
---
+++ src/Infrastructure/Data/Configurations/PokemonSpeciesConfiguration.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PokemonInHomeAPI.Domain.Entities;
+
+namespace PokemonInHomeAPI.Infrastructure.Data.Configurations;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PokemonInHomeAPI.Domain.Entities;
+
+public class PokemonSpeciesConfiguration : IEntityTypeConfiguration<PokemonSpecies>
+{
+    public void Configure(EntityTypeBuilder<PokemonSpecies> builder)
+    {
+        builder.HasKey(p => p.Id);
+        
+        builder.Property(p => p.Name)
+            .HasMaxLength(50)
+            .IsRequired();
+
+        builder.Property(p => p.Type1)
+            .IsRequired();
+
+        builder.Property(p => p.BaseHp)
+            .IsRequired();
+
+        builder.Property(p => p.BaseAttack)
+            .IsRequired();
+
+        builder.Property(p => p.BaseDefense)
+            .IsRequired();
+
+        builder.Property(p => p.BaseSpecialAttack)
+            .IsRequired();
+
+        builder.Property(p => p.BaseSpecialDefense)
+            .IsRequired();
+
+        builder.Property(p => p.BaseSpeed)
+            .IsRequired();
+        
+        builder.HasMany(p => p.Pokemons)
+            .WithOne(p => p.Species)
+            .HasForeignKey(p => p.SpeciesId);
+
+        builder.HasMany(p => p.Pokedexes)
+            .WithOne(p => p.Species)
+            .HasForeignKey(p => p.SpeciesId);
+    }
+}
+
