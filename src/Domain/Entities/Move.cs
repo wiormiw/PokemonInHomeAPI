ef9eb568b79616b2b9fcd3af5a7b558218852b7e
Wijoyo Raharjo Murti Indra Wahyono <wwijoyo2@gmail.com>
2025-05-27 13:02:05 +0700

    feat: Move Entity + Configuration

---
+++ src/Domain/Entities/Move.cs
@@ -0,0 +1,16 @@
+﻿namespace PokemonInHomeAPI.Domain.Entities;
+
+public class Move : BaseAuditableEntity
+{
+    public string Name { get; set; } = string.Empty;
+    
+    public PokemonType Type { get; set; } = new PokemonType("Unknown");
+    
+    public int? Power { get; set; }
+    
+    public int? Accuracy { get; set; }
+
+    public int Pp { get; set; }
+    
+    public ICollection<PokemonMove> PokemonMoves { get; set; } = new List<PokemonMove>();
+}
---
+++ src/Infrastructure/Data/Configurations/MoveConfiguration.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PokemonInHomeAPI.Domain.Entities;
+
+namespace PokemonInHomeAPI.Infrastructure.Data.Configurations;
+
+public class MoveConfiguration : IEntityTypeConfiguration<Move>
+{
+    public void Configure(EntityTypeBuilder<Move> builder)
+    {
+        builder.HasKey(m => m.Id);
+
+        builder.Property(m => m.Name)
+            .IsRequired()
+            .HasMaxLength(50);
+
+        builder.Property(m => m.Type)
+            .IsRequired();
+
+        builder.Property(m => m.Power);
+
+        builder.Property(m => m.Accuracy)
+            .HasPrecision(3, 0); // Optional, accuracy is 0–100
+
+        builder.Property(m => m.Pp)
+            .IsRequired();
+
+        builder.HasMany(m => m.PokemonMoves)
+            .WithOne(pm => pm.Move)
+            .HasForeignKey(pm => pm.MoveId);
+    }
+}
+
