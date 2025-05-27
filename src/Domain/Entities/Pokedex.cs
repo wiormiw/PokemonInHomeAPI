eb60b72d1de80dccd3c6f11c01caf15eb9b5a69a
Wijoyo Raharjo Murti Indra Wahyono <wwijoyo2@gmail.com>
2025-05-27 12:57:13 +0700

    feat: Pokedex Entity + Configuration

---
+++ src/Domain/Entities/Pokedex.cs
@@ -0,0 +1,16 @@
+﻿namespace PokemonInHomeAPI.Domain.Entities;
+
+public class Pokedex : BaseAuditableEntity
+{
+    public int PlayerId { get; set; }
+    
+    public int SpeciesId { get; set; }
+
+    public bool Seen { get; set; } = false;
+
+    public bool Caught { get; set; } = false;
+
+    public Player? Player { get; set; }
+    
+    public PokemonSpecies? Species { get; set; }
+}
---
+++ src/Infrastructure/Data/Configurations/PokedexConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PokemonInHomeAPI.Domain.Entities;
+
+namespace PokemonInHomeAPI.Infrastructure.Data.Configurations;
+
+public class PokedexConfiguration: IEntityTypeConfiguration<Pokedex>
+{
+    public void Configure(EntityTypeBuilder<Pokedex> builder)
+    {
+        builder.HasKey(pd => new { pd.PlayerId, pd.SpeciesId });
+        
+        builder.HasOne(pd => pd.Player)
+            .WithMany(p => p.Pokedexes)
+            .HasForeignKey(pd => pd.PlayerId);
+        
+        builder.HasOne(pd => pd.Species)
+            .WithMany(s => s.Pokedexes)
+            .HasForeignKey(pd => pd.SpeciesId);
+    }
+    
+}
