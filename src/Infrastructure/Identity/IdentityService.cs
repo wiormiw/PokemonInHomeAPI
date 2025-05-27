6d025b7c7e9a2e0204539b2164b08b9ae100601e
Wijoyo Raharjo Murti Indra Wahyono <wwijoyo2@gmail.com>
2025-05-27 15:33:21 +0700

    feat: Attach Player Entity (IndentityUser Extensions Entity) when create

--- src/Infrastructure/Identity/IdentityService.cs
+++ src/Infrastructure/Identity/IdentityService.cs
@@ -3,6 +3,8 @@ using PokemonInHomeAPI.Application.Common.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using PokemonInHomeAPI.Domain.Entities;
+using PokemonInHomeAPI.Infrastructure.Data;
 
 namespace PokemonInHomeAPI.Infrastructure.Identity;
 
@@ -11,15 +13,18 @@ public class IdentityService : IIdentityService
     private readonly UserManager<ApplicationUser> _userManager;
     private readonly IUserClaimsPrincipalFactory<ApplicationUser> _userClaimsPrincipalFactory;
     private readonly IAuthorizationService _authorizationService;
+    private readonly ApplicationDbContext _context;
 
     public IdentityService(
         UserManager<ApplicationUser> userManager,
         IUserClaimsPrincipalFactory<ApplicationUser> userClaimsPrincipalFactory,
-        IAuthorizationService authorizationService)
+        IAuthorizationService authorizationService,
+        ApplicationDbContext context)
     {
         _userManager = userManager;
         _userClaimsPrincipalFactory = userClaimsPrincipalFactory;
         _authorizationService = authorizationService;
+        _context = context;
     }
 
     public async Task<string?> GetUserNameAsync(string userId)
@@ -39,6 +44,14 @@ public class IdentityService : IIdentityService
 
         var result = await _userManager.CreateAsync(user, password);
 
+        if (result.Succeeded)
+        {
+            var playerData = new Player { ApplicationUserId = user.Id};
+            
+            await _context.Players.AddAsync(playerData);
+            await _context.SaveChangesAsync();
+        }
+        
         return (result.ToApplicationResult(), user.Id);
     }
 
