ecd98539de3acb1f0e9e8efbfe7af126682ea772
Wijoyo Raharjo Murti Indra Wahyono <wwijoyo2@gmail.com>
2025-05-27 13:02:46 +0700

    feat: PlayerItem Entity + Configuration

---
+++ src/Domain/Entities/PlayerItem.cs
@@ -0,0 +1,14 @@
+﻿namespace PokemonInHomeAPI.Domain.Entities;
+
+public class PlayerItem
+{
+    public int PlayerId { get; set; }
+    
+    public int ItemId { get; set; } 
+    
+    public int Quantity { get; set; }
+
+    public Player? Player { get; set; }
+    
+    public Item? Item { get; set; }
+}
---
+++ src/Infrastructure/Data/Configurations/PlayerItemConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PokemonInHomeAPI.Domain.Entities;
+
+namespace PokemonInHomeAPI.Infrastructure.Data.Configurations;
+
+public class PlayerItemConfiguration : IEntityTypeConfiguration<PlayerItem>
+{
+    public void Configure(EntityTypeBuilder<PlayerItem> builder)
+    {
+        builder.HasKey(pi => new { pi.PlayerId, pi.ItemId });
+
+        builder.Property(pi => pi.Quantity)
+            .IsRequired();
+
+        builder.HasOne(pi => pi.Player)
+            .WithMany(p => p.PlayerItems)
+            .HasForeignKey(pi => pi.PlayerId);
+
+        builder.HasOne(pi => pi.Item)
+            .WithMany(i => i.PlayerItems)
+            .HasForeignKey(pi => pi.ItemId);
+    }
+}
+
