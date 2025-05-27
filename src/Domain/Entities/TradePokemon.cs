dd85ce36bf7793b2136f3843d7e2a70079f8ecc6
Wijoyo Raharjo Murti Indra Wahyono <wwijoyo2@gmail.com>
2025-05-27 13:03:11 +0700

    feat: TradePokemon Entity + Configuration

---
+++ src/Domain/Entities/TradePokemon.cs
@@ -0,0 +1,16 @@
+﻿namespace PokemonInHomeAPI.Domain.Entities;
+
+public class TradePokemon
+{
+    public int TradeId { get; set; }
+    
+    public int PokemonId { get; set; }
+    
+    public int PlayerId { get; set; }
+
+    public Trade? Trade { get; set; }
+
+    public Pokemon? Pokemon { get; set; }
+    
+    public Player? Player { get; set; }
+}
---
+++ src/Infrastructure/Data/Configurations/TradePokemonConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PokemonInHomeAPI.Domain.Entities;
+
+namespace PokemonInHomeAPI.Infrastructure.Data.Configurations;
+
+public class TradePokemonConfiguration : IEntityTypeConfiguration<TradePokemon>
+{
+    public void Configure(EntityTypeBuilder<TradePokemon> builder)
+    {
+        builder.HasKey(tp => new { tp.TradeId, tp.PokemonId });
+
+        builder.Property(tp => tp.PlayerId)
+            .IsRequired();
+
+        builder.HasOne(tp => tp.Trade)
+            .WithMany(t => t.TradePokemons)
+            .HasForeignKey(tp => tp.TradeId);
+
+        builder.HasOne(tp => tp.Pokemon)
+            .WithMany(p => p.TradePokemons)
+            .HasForeignKey(tp => tp.PokemonId);
+
+        builder.HasOne(tp => tp.Player)
+            .WithMany()
+            .HasForeignKey(tp => tp.PlayerId);
+    }
+}
+
