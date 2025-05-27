b39a48cdc8c8c6dbd122d9edf7e87a3187b05c60
Wijoyo Raharjo Murti Indra Wahyono <wwijoyo2@gmail.com>
2025-05-27 12:58:07 +0700

    feat: Pokemon Entity + Configuration

---
+++ src/Domain/Entities/Pokemon.cs
@@ -0,0 +1,39 @@
+﻿namespace PokemonInHomeAPI.Domain.Entities;
+
+public class Pokemon : BaseAuditableEntity
+{
+    public int SpeciesId { get; set; }
+    
+    public int Level { get; set; }
+    
+    public int IvHp { get; set; }
+    
+    public int IvAttack { get; set; }
+    
+    public int IvDefense { get; set; }
+    
+    public int IvSpeed { get; set; }
+    
+    public int IvSpecialAttack { get; set; }
+    
+    public int IvSpecialDefense { get; set; }
+    
+    public int EvHp { get; set; }
+    
+    public int EvDefense { get; set; }
+    
+    public int EvSpecialAttack { get; set; }
+    
+    public int EvSpecialDefense { get; set; }
+    
+    public int EvSpeed { get; set; }
+    
+    public int CurrentHp { get; set; }
+    
+    public PokemonSpecies? Species { get; set; }
+    
+    public ICollection<PlayerPokemon> PlayerPokemons { get; set; } = new List<PlayerPokemon>();
+    public ICollection<PokemonMove> PokemonMoves { get; set; } = new List<PokemonMove>();
+    public ICollection<BattlePokemon> BattlePokemons { get; set; } = new List<BattlePokemon>();
+    public ICollection<TradePokemon> TradePokemons { get; set; } = new List<TradePokemon>();
+}
---
+++ src/Infrastructure/Data/Configurations/PokemonConfiguration.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PokemonInHomeAPI.Domain.Entities;
+
+public class PokemonConfiguration : IEntityTypeConfiguration<Pokemon>
+{
+    public void Configure(EntityTypeBuilder<Pokemon> builder)
+    {
+        builder.HasKey(p => p.Id);
+
+        builder.Property(p => p.Level)
+            .IsRequired();
+
+        builder.Property(p => p.IvHp).IsRequired();
+        builder.Property(p => p.IvAttack).IsRequired();
+        builder.Property(p => p.IvDefense).IsRequired();
+        builder.Property(p => p.IvSpecialAttack).IsRequired();
+        builder.Property(p => p.IvSpecialDefense).IsRequired();
+        builder.Property(p => p.IvSpeed).IsRequired();
+
+        builder.Property(p => p.CurrentHp).IsRequired();
+
+        builder.HasOne(p => p.Species)
+            .WithMany(s => s.Pokemons)
+            .HasForeignKey(p => p.SpeciesId)
+            .IsRequired();
+
+        // Configure collections (optional)
+        builder.HasMany(p => p.PlayerPokemons)
+            .WithOne(pp => pp.Pokemon)
+            .HasForeignKey(pp => pp.PokemonId);
+
+        builder.HasMany(p => p.PokemonMoves)
+            .WithOne(pm => pm.Pokemon)
+            .HasForeignKey(pm => pm.PokemonId);
+
+        builder.HasMany(p => p.BattlePokemons)
+            .WithOne(bp => bp.Pokemon)
+            .HasForeignKey(bp => bp.PokemonId);
+
+        builder.HasMany(p => p.TradePokemons)
+            .WithOne(tp => tp.Pokemon)
+            .HasForeignKey(tp => tp.PokemonId);
+    }
+}
