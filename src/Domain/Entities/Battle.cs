ccbd539d0b07a381c52bca50febf44267c548df9
Wijoyo Raharjo Murti Indra Wahyono <wwijoyo2@gmail.com>
2025-05-27 12:58:36 +0700

    feat: Battle Entity + Configuration

---
+++ src/Domain/Entities/Battle.cs
@@ -0,0 +1,20 @@
+﻿namespace PokemonInHomeAPI.Domain.Entities;
+
+public class Battle : BaseAuditableEntity
+{
+    public int Player1Id { get; set; }
+    
+    public int Player2Id { get; set; }
+    
+    public int? WinnerId { get; set; }
+    
+    public DateTimeOffset BattleDate { get; set; } = DateTimeOffset.Now;
+    
+    public Player? Player1 { get; set; }
+    
+    public Player? Player2 { get; set; }
+    
+    public Player? Winner { get; set; }
+    
+    public ICollection<BattlePokemon> BattlePokemons { get; set; } = new List<BattlePokemon>();
+}
---
+++ src/Infrastructure/Data/Configurations/BattleConfiguration.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PokemonInHomeAPI.Domain.Entities;
+
+namespace PokemonInHomeAPI.Infrastructure.Data.Configurations;
+
+public class BattleConfiguration : IEntityTypeConfiguration<Battle>
+{
+    public void Configure(EntityTypeBuilder<Battle> builder)
+    {
+        builder.HasKey(b => b.Id);
+
+        builder.Property(b => b.BattleDate)
+            .IsRequired();
+
+        builder.HasOne(b => b.Player1)
+            .WithMany(p => p.Player1Battles)
+            .HasForeignKey(b => b.Player1Id)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        builder.HasOne(b => b.Player2)
+            .WithMany(p => p.Player2Battles)
+            .HasForeignKey(b => b.Player2Id)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        builder.HasOne(b => b.Winner)
+            .WithMany()
+            .HasForeignKey(b => b.WinnerId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        builder.HasMany(b => b.BattlePokemons)
+            .WithOne(bp => bp.Battle)
+            .HasForeignKey(bp => bp.BattleId);
+    }
+}
+
