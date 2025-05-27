ab513f12a0b34fb00b120a1bdedbe929c8bd500b
Wijoyo Raharjo Murti Indra Wahyono <wwijoyo2@gmail.com>
2025-05-27 12:58:24 +0700

    feat: Item Entity + Configuration

---
+++ src/Domain/Entities/Item.cs
@@ -0,0 +1,12 @@
+﻿namespace PokemonInHomeAPI.Domain.Entities;
+
+public class Item : BaseAuditableEntity
+{
+    public string Name { get; set; } = string.Empty;
+    
+    public string Description { get; set; } = string.Empty;
+
+    public ItemType Type { get; set; }
+    
+    public ICollection<PlayerItem> PlayerItems { get; set; } = new List<PlayerItem>();
+}
---
+++ src/Infrastructure/Data/Configurations/ItemConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PokemonInHomeAPI.Domain.Entities;
+
+namespace PokemonInHomeAPI.Infrastructure.Data.Configurations;
+
+public class ItemConfiguration : IEntityTypeConfiguration<Item>
+{
+    public void Configure(EntityTypeBuilder<Item> builder)
+    {
+        builder.HasKey(i => i.Id);
+
+        builder.Property(i => i.Name)
+            .IsRequired()
+            .HasMaxLength(50);
+
+        builder.Property(i => i.Description); // Optional
+
+        builder.Property(i => i.Type)
+            .IsRequired();
+
+        builder.HasMany(i => i.PlayerItems)
+            .WithOne(pi => pi.Item)
+            .HasForeignKey(pi => pi.ItemId);
+    }
+}
+
