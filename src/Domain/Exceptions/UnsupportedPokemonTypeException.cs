efda25d92835399b747ed875f3493ba145b359fc
Wijoyo Raharjo Murti Indra Wahyono <wwijoyo2@gmail.com>
2025-05-31 17:07:02 +0700

    feat: Add custom exception for unsupported pokemon type

---
+++ src/Domain/Exceptions/UnsupportedPokemonTypeException.cs
@@ -0,0 +1,11 @@
+﻿namespace PokemonInHomeAPI.Domain.Exceptions;
+
+public class UnsupportedPokemonTypeException : Exception
+{
+    
+    public UnsupportedPokemonTypeException(string pokemonType)
+        : base($"Pokemon type \"{pokemonType}\" is not exists.")
+    {
+    }
+    
+}
