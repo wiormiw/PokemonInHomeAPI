a2e7f6a42c17f04ee7115fe067d63f2051433e87
Wijoyo Raharjo Murti Indra Wahyono <wwijoyo2@gmail.com>
2025-05-31 17:02:27 +0700

    feat: Register all entity in IApplicationDbContext.cs for Entity Core Framework

--- src/Application/Common/Interfaces/IApplicationDbContext.cs
+++ src/Application/Common/Interfaces/IApplicationDbContext.cs
@@ -8,5 +8,31 @@ public interface IApplicationDbContext
 
     DbSet<TodoItem> TodoItems { get; }
 
+    DbSet<Player> Players { get; }
+
+    DbSet<PokemonSpecies> PokemonSpecies { get; }
+
+    DbSet<Pokemon> Pokemons { get; }
+
+    DbSet<PlayerPokemon> PlayerPokemons { get; }
+
+    DbSet<Pokedex> Pokedexes { get; }
+
+    DbSet<Move> Moves { get; }
+
+    DbSet<PokemonMove> PokemonMoves { get; }
+
+    DbSet<Item> Items { get; }
+
+    DbSet<PlayerItem> PlayerItems { get; }
+
+    DbSet<Battle> Battles { get; }
+    
+    DbSet<BattlePokemon> BattlePokemons { get; }
+    
+    DbSet<Trade> Trades { get; }
+    
+    DbSet<TradePokemon> TradePokemons { get; }
+    
     Task<int> SaveChangesAsync(CancellationToken cancellationToken);
 }
