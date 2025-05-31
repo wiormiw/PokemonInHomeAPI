2ae033d231c1b97a4ee7235ab6bc7b618924f7d9
Wijoyo Raharjo Murti Indra Wahyono <wwijoyo2@gmail.com>
2025-05-31 17:09:23 +0700

    feat: Create Pokemon command

---
+++ src/Application/Pokemons/Commands/CreatePokemon/CreatePokemon.cs
@@ -0,0 +1,67 @@
+﻿using PokemonInHomeAPI.Application.Common.Interfaces;
+using PokemonInHomeAPI.Application.Common.Security;
+using PokemonInHomeAPI.Domain.Constants;
+using PokemonInHomeAPI.Domain.Entities;
+using PokemonInHomeAPI.Domain.Events;
+using PokemonInHomeAPI.Domain.ValueObjects;
+
+namespace PokemonInHomeAPI.Application.Pokemons.Commands.CreatePokemon;
+
+[Authorize(Roles = Roles.Administrator)]
+public record CreatePokemonCommand : IRequest<int>
+{
+    public string? Name { get; init; }
+    
+    public string? Type1 { get; init; }
+    
+    public string? Type2 { get; init; }
+   
+    public int BaseHp { get; init; }
+    
+    public int BaseAttack { get; init; }
+    
+    public int BaseDefense { get; init; }
+    
+    public int BaseSpecialAttack { get; init; }
+    
+    public int BaseSpecialDefense { get; init; }
+    
+    public int BaseSpeed { get; init; }
+}
+
+public class CreatePokemonCommandHandler : IRequestHandler<CreatePokemonCommand, int>
+{
+    private readonly IApplicationDbContext _context;
+
+    public CreatePokemonCommandHandler(IApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<int> Handle(CreatePokemonCommand request, CancellationToken cancellationToken)
+    {
+        ArgumentNullException.ThrowIfNull(request.Name);
+        ArgumentNullException.ThrowIfNull(request.Type1);
+        
+        var entity = new PokemonSpecies
+        {
+            Name = request.Name,
+            Type1 = PokemonType.From(request.Type1),
+            Type2 = request.Type2 != null ? PokemonType.From(request.Type2) : null,
+            BaseHp = request.BaseHp,
+            BaseAttack = request.BaseAttack,
+            BaseDefense = request.BaseDefense,
+            BaseSpecialAttack = request.BaseSpecialAttack,
+            BaseSpecialDefense = request.BaseSpecialDefense,
+            BaseSpeed = request.BaseSpeed,
+        };
+        
+        entity.AddDomainEvent(new PokemonCreatedEvent(entity));
+        
+        _context.PokemonSpecies.Add(entity);
+        
+        await _context.SaveChangesAsync(cancellationToken);
+        
+        return entity.Id;
+    }
+}
---
+++ src/Application/Pokemons/Commands/CreatePokemon/CreatePokemonCommandValidator.cs
@@ -0,0 +1,62 @@
+﻿using FluentValidation.Validators;
+using PokemonInHomeAPI.Domain.Constants;
+using PokemonInHomeAPI.Domain.ValueObjects;
+
+namespace PokemonInHomeAPI.Application.Pokemons.Commands.CreatePokemon;
+
+public class CreatePokemonCommandValidator : AbstractValidator<CreatePokemonCommand>
+{
+    public CreatePokemonCommandValidator()
+    {
+        RuleFor(v => v.Name)
+            .MaximumLength(255)
+            .WithMessage(string.Format(ErrorMessage.AttributeLengthError, "Name", 255))
+            .NotEmpty()
+            .WithMessage(string.Format(ErrorMessage.EmptyAttributeError, "Name"));
+        
+        RuleFor(v => v.Type1)
+            .NotEmpty().WithMessage(string.Format(ErrorMessage.EmptyAttributeError, "Type1"))
+            .Must(t => PokemonType.SupportedTypes.Any(st => st.Name == t))
+            .WithMessage(string.Format(ErrorMessage.PokemonUnsupportedError, "Type1"));
+        
+        RuleFor(v => v.Type2)
+            .Must(t => t == null || PokemonType.SupportedTypes.Any(st => st.Name == t))
+            .WithMessage(string.Format(ErrorMessage.PokemonUnsupportedError, "Type2"));
+
+        RuleFor(v => v.BaseHp)
+            .GreaterThan(0)
+            .WithMessage(string.Format(ErrorMessage.MustPositiveAttributeError, "BaseHp"))
+            .LessThanOrEqualTo(255)
+            .WithMessage(string.Format(ErrorMessage.LessThanOrEqualToAttributeError, "BaseHp", 255));
+        
+        RuleFor(v => v.BaseAttack)
+            .GreaterThan(0)
+            .WithMessage(string.Format(ErrorMessage.MustPositiveAttributeError, "BaseAttack"))
+            .LessThanOrEqualTo(255)
+            .WithMessage(string.Format(ErrorMessage.LessThanOrEqualToAttributeError, "BaseAttack", 255));
+        
+        RuleFor(v => v.BaseDefense)
+            .GreaterThan(0)
+            .WithMessage(string.Format(ErrorMessage.MustPositiveAttributeError, "BaseDefence"))
+            .LessThanOrEqualTo(255)
+            .WithMessage(string.Format(ErrorMessage.LessThanOrEqualToAttributeError, "BaseDefence", 255));;
+        
+        RuleFor(v => v.BaseSpecialAttack)
+            .GreaterThan(0)
+            .WithMessage(string.Format(ErrorMessage.MustPositiveAttributeError, "BaseSpecialAttack"))
+            .LessThanOrEqualTo(255)
+            .WithMessage(string.Format(ErrorMessage.LessThanOrEqualToAttributeError, "BaseSpecialAttack", 255));;
+        
+        RuleFor(v => v.BaseSpecialDefense)
+            .GreaterThan(0)
+            .WithMessage(string.Format(ErrorMessage.MustPositiveAttributeError, "BaseSpecialDefence"))
+            .LessThanOrEqualTo(255)
+            .WithMessage(string.Format(ErrorMessage.LessThanOrEqualToAttributeError, "BaseSpecialDefence", 255));;
+        
+        RuleFor(v => v.BaseSpeed)
+            .GreaterThan(0)
+            .WithMessage(string.Format(ErrorMessage.MustPositiveAttributeError, "BaseSpeed"))
+            .LessThanOrEqualTo(255)
+            .WithMessage(string.Format(ErrorMessage.LessThanOrEqualToAttributeError, "BaseSpeed", 255));
+    }
+}
