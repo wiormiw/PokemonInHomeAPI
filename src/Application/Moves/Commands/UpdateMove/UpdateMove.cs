0894dabd387e5343bb1ca920020680391977bb15
Wijoyo Raharjo Murti Indra Wahyono <wwijoyo2@gmail.com>
2025-06-03 22:24:49 +0700

    feat: Update Move Command

---
+++ src/Application/Moves/Commands/UpdateMove/UpdateMove.cs
@@ -0,0 +1,54 @@
+﻿using PokemonInHomeAPI.Application.Common.Interfaces;
+using PokemonInHomeAPI.Domain.ValueObjects;
+
+namespace PokemonInHomeAPI.Application.Moves.Commands.UpdateMove;
+
+public record UpdateMoveCommand : IRequest
+{
+    public int Id { get; init; }
+    
+    public string? Name { get; init; }
+    
+    public string? Type { get; init; }
+    
+    public int? Power { get; init; }
+    
+    public int? Accuracy { get; init; }
+
+    public int? Pp { get; init; }
+}
+
+public class UpdateMoveCommandHandler : IRequestHandler<UpdateMoveCommand>
+{
+    private readonly IApplicationDbContext _context;
+
+    public UpdateMoveCommandHandler(IApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task Handle(UpdateMoveCommand request, CancellationToken cancellationToken)
+    {
+        var entity = await _context.Moves
+            .FindAsync(new object[] { request.Id }, cancellationToken);
+        
+        Guard.Against.NotFound(request.Id, entity);
+
+        if (request.Name != null)
+            entity.Name = request.Name;
+        
+        if (request.Type != null)
+            entity.Type = PokemonType.From(request.Type);
+        
+        if (request.Power.HasValue)
+            entity.Power = request.Power.Value;
+        
+        if (request.Accuracy.HasValue)
+            entity.Accuracy = request.Accuracy.Value;
+        
+        if (request.Pp.HasValue)
+            entity.Pp = request.Pp.Value;
+        
+        await _context.SaveChangesAsync(cancellationToken);
+    }
+}
---
+++ src/Application/Moves/Commands/UpdateMove/UpdateMoveCommandValidator.cs
@@ -0,0 +1,60 @@
+using PokemonInHomeAPI.Application.Common.Interfaces;
+using PokemonInHomeAPI.Domain.Constants;
+using PokemonInHomeAPI.Domain.ValueObjects;
+
+namespace PokemonInHomeAPI.Application.Moves.Commands.UpdateMove;
+
+public class UpdateMoveCommandValidator : AbstractValidator<UpdateMoveCommand>
+{
+    private readonly IApplicationDbContext _context;
+    
+    public UpdateMoveCommandValidator(IApplicationDbContext context)
+    {
+        _context = context;
+
+        RuleFor(v => v.Id)
+            .GreaterThan(0)
+            .WithMessage(ValidationMessage.PositiveMessage);
+        
+        RuleFor(v => v.Name)
+            .NotEmpty()
+            .WithMessage(ValidationMessage.RequiredMessage)
+            .MaximumLength(255)
+            .WithMessage(ValidationMessage.MaxLength255Message)
+            .MustAsync(BeUniqueName)
+            .WithMessage(ValidationMessage.UniqueMessage)
+            .WithErrorCode("Unique");
+
+        RuleFor(v => v.Type)
+            .NotEmpty()
+            .WithMessage(ValidationMessage.RequiredMessage)
+            .Must(t => t == null || PokemonType.SupportedTypes.Any(st => st.Name == t))
+            .WithMessage(ValidationMessage.UnsupportedTypeMessage);
+        
+        RuleFor(v => v.Power)
+            .GreaterThan(0).When(v => v.Power.HasValue)
+            .WithMessage(ValidationMessage.PositiveMessage)
+            .LessThanOrEqualTo(255).When(v => v.Power.HasValue)
+            .WithMessage(ValidationMessage.MaxValue255Message);
+        
+        RuleFor(v => v.Accuracy)
+            .GreaterThan(0).When(v => v.Power.HasValue)
+            .WithMessage(ValidationMessage.PositiveMessage)
+            .LessThanOrEqualTo(255).When(v => v.Power.HasValue)
+            .WithMessage(ValidationMessage.MaxValue255Message);
+        
+        RuleFor(v => v.Pp)
+            .GreaterThan(0).When(v => v.Power.HasValue)
+            .WithMessage(ValidationMessage.PositiveMessage)
+            .LessThanOrEqualTo(255).When(v => v.Power.HasValue)
+            .WithMessage(ValidationMessage.MaxValue255Message);
+    }
+    
+    public async Task<bool> BeUniqueName(UpdateMoveCommand model, string name,
+        CancellationToken cancellationToken)
+    {
+        return !await _context.Moves
+            .Where(mv => mv.Id != model.Id)
+            .AnyAsync(mv => mv.Name == name, cancellationToken);
+    }
+}
