843cd8f9d539dbbeac1418dade8cc8780509cb06
Wijoyo Raharjo Murti Indra Wahyono <wwijoyo2@gmail.com>
2025-06-03 22:23:09 +0700

    feat: Delete Move Command

---
+++ src/Application/Moves/Commands/DeleteMove/DeleteMove.cs
@@ -0,0 +1,30 @@
+﻿using PokemonInHomeAPI.Application.Common.Interfaces;
+using PokemonInHomeAPI.Domain.Events;
+
+namespace PokemonInHomeAPI.Application.Moves.Commands.DeleteMove;
+
+public record DeleteMoveCommand(int Id) : IRequest;
+
+public class DeleteMoveCommandHandler : IRequestHandler<DeleteMoveCommand>
+{
+    private readonly IApplicationDbContext _context;
+
+    public DeleteMoveCommandHandler(IApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task Handle(DeleteMoveCommand request, CancellationToken cancellationToken)
+    {
+        var entity = await _context.Moves
+            .FindAsync(new object[] { request.Id }, cancellationToken);
+        
+        Guard.Against.NotFound(request.Id, entity);
+        
+        _context.Moves.Remove(entity);
+
+        entity.AddDomainEvent(new MoveDeletedEvent(entity));
+
+        await _context.SaveChangesAsync(cancellationToken);
+    }
+}
