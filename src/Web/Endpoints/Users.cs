650ccc690f14b56fc2df362e9ee7fc8ee759ad43
Wijoyo Raharjo Murti Indra Wahyono <wwijoyo2@gmail.com>
2025-06-04 02:55:21 +0700

    style: Remove unused commented code.

--- src/Web/Endpoints/Users.cs
+++ src/Web/Endpoints/Users.cs
@@ -9,41 +9,5 @@ public class Users : EndpointGroupBase
         var group = app.MapGroup(this);
 
         group.MapIdentityApiCustom<ApplicationUser>();
-
-        // group.MapPost("/register", async (
-        //         [FromBody] RegisterRequest request,
-        //         UserManager<ApplicationUser> userManager,
-        //         RoleManager<IdentityRole> roleManager) =>
-        //     {
-        //         var user = new ApplicationUser
-        //         {
-        //             UserName = request.Email,
-        //             Email = request.Email
-        //         };
-        //
-        //         var result = await userManager.CreateAsync(user, request.Password);
-        //
-        //         if (!result.Succeeded)
-        //         {
-        //             return Results.BadRequest(result.Errors);
-        //         }
-        //
-        //         // Ensure the role exists before assigning
-        //         var roleAssign = new[] { Roles.User, Roles.Player };
-        //
-        //         foreach (var role in roleAssign)
-        //         {
-        //             if (!await roleManager.RoleExistsAsync(role))
-        //             {
-        //                 await roleManager.CreateAsync(new IdentityRole(role));
-        //             }
-        //
-        //             await userManager.AddToRoleAsync(user, role);   
-        //         }
-        //
-        //         return Results.Ok("User registered with role 'User'");
-        //     })
-        //     .AllowAnonymous()
-        //     .WithName("CustomRegister");
     }
 }
