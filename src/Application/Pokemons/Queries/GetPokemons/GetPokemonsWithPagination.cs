4a56de959d995388b675542347a6e8af519f9bdf
Wijoyo Raharjo Murti Indra Wahyono <wwijoyo2@gmail.com>
2025-06-03 17:38:37 +0700

    fix: Remove id from Get Pokemon with Pagination

--- src/Application/Pokemons/Queries/GetPokemons/GetPokemonsWithPagination.cs
+++ src/Application/Pokemons/Queries/GetPokemons/GetPokemonsWithPagination.cs
@@ -1,21 +1,18 @@
 using PokemonInHomeAPI.Application.Common.Interfaces;
 using PokemonInHomeAPI.Application.Common.Mappings;
 using PokemonInHomeAPI.Application.Common.Models;
-using PokemonInHomeAPI.Application.Pokemons.Queries.GetPokemons;
-using PokemonInHomeAPI.Application.TodoItems.Queries.GetTodoItemsWithPagination;
+using PokemonInHomeAPI.Application.Common.Security;
 
 namespace PokemonInHomeAPI.Application.Pokemons.Queries.GetPokemons;
 
+[Authorize]
 public record GetPokemonsWithPaginationQuery : IRequest<PaginatedList<PokemonDto>>
 {
-    public int PokemonId { get; init; }
     public int PageNumber { get; init; } = 1;
     public int PageSize { get; init; } = 10;
 }
 
-public class
-    GetPokemonsWithPaginationQueryHandler : IRequestHandler<GetPokemonsWithPaginationQuery,
-    PaginatedList<PokemonDto>>
+public class GetPokemonsWithPaginationQueryHandler : IRequestHandler<GetPokemonsWithPaginationQuery, PaginatedList<PokemonDto>>
 {
     private readonly IApplicationDbContext _context;
     private readonly IMapper _mapper;
@@ -26,13 +23,11 @@ public class
         _mapper = mapper;
     }
 
-    public async Task<PaginatedList<PokemonDto>> Handle(GetPokemonsWithPaginationQuery request,
-        CancellationToken cancellationToken)
+    public async Task<PaginatedList<PokemonDto>> Handle(GetPokemonsWithPaginationQuery request, CancellationToken cancellationToken)
     {
         return await _context.PokemonSpecies
-            .Where(x => x.Id == request.PokemonId)
             .OrderBy(x => x.Name)
             .ProjectTo<PokemonDto>(_mapper.ConfigurationProvider)
-            .PaginatedListAsync(request.PageNumber, request.PageSize, cancellationToken);
+            .PaginatedListAsync(request.PageNumber, request.PageSize);
     }
 }
