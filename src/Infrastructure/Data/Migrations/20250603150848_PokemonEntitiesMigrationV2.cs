cf8531f24012ea7648560011474b10481d1bc643
Wijoyo Raharjo Murti Indra Wahyono <wwijoyo2@gmail.com>
2025-06-03 22:17:46 +0700

    fix: Set Moves power and accuracy to non-nullable

---
+++ src/Infrastructure/Data/Migrations/20250603150848_PokemonEntitiesMigrationV2.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PokemonInHomeAPI.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class PokemonEntitiesMigrationV2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Power",
+                table: "Moves",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Accuracy",
+                table: "Moves",
+                type: "integer",
+                precision: 3,
+                scale: 0,
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldPrecision: 3,
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Power",
+                table: "Moves",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Accuracy",
+                table: "Moves",
+                type: "integer",
+                precision: 3,
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldPrecision: 3,
+                oldScale: 0);
+        }
+    }
+}
