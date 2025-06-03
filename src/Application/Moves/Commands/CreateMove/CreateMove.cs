3b217580d2951b490845003a0b68d03470f9a386
Wijoyo Raharjo Murti Indra Wahyono <wwijoyo2@gmail.com>
2025-06-03 22:23:00 +0700

    feat: Create Move Command

---
+++ src/Application/Moves/Commands/CreateMove/CreateMove.cs
@@ -0,0 +1,55 @@
+﻿using PokemonInHomeAPI.Application.Common.Interfaces;
+using PokemonInHomeAPI.Application.Common.Security;
+using PokemonInHomeAPI.Domain.Constants;
+using PokemonInHomeAPI.Domain.Entities;
+using PokemonInHomeAPI.Domain.Events;
+using PokemonInHomeAPI.Domain.ValueObjects;
+
+namespace PokemonInHomeAPI.Application.Moves.Commands.CreateMove;
+
+[Authorize(Roles = Roles.Administrator)]
+public record CreateMoveCommand : IRequest<int>
+{
+    public string? Name { get; init; }
+    
+    public string? Type { get; init; }
+    
+    public int Power { get; init; }
+    
+    public int Accuracy { get; init; }
+
+    public int Pp { get; init; }
+}
+
+public class CreateMoveCommandHandler : IRequestHandler<CreateMoveCommand, int>
+{
+    private readonly IApplicationDbContext _context;
+
+    public CreateMoveCommandHandler(IApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<int> Handle(CreateMoveCommand request, CancellationToken cancellationToken)
+    {
+        ArgumentNullException.ThrowIfNull(request.Name);
+        ArgumentNullException.ThrowIfNull(request.Type);
+
+        var entity = new Move
+        {
+            Name = request.Name,
+            Type = PokemonType.From(request.Type),
+            Power = request.Power,
+            Accuracy = request.Accuracy,
+            Pp = request.Pp,
+        };
+            
+        entity.AddDomainEvent(new MoveCreatedEvent(entity));
+        
+        _context.Moves.Add(entity);
+        
+        await _context.SaveChangesAsync(cancellationToken);
+        
+        return entity.Id;
+    }
+}
---
+++ src/Application/Moves/Commands/CreateMove/CreateMoveCommandValidator.cs
@@ -0,0 +1,55 @@
+using PokemonInHomeAPI.Application.Common.Interfaces;
+using PokemonInHomeAPI.Domain.Constants;
+using PokemonInHomeAPI.Domain.ValueObjects;
+
+namespace PokemonInHomeAPI.Application.Moves.Commands.CreateMove;
+
+public class CreateMoveCommandValidator : AbstractValidator<CreateMoveCommand>
+{
+    private readonly IApplicationDbContext _context;
+    
+    public CreateMoveCommandValidator(IApplicationDbContext context)
+    {
+        _context = context;
+        
+        RuleFor(v => v.Name)
+            .NotEmpty()
+            .WithMessage(ValidationMessage.RequiredMessage)
+            .MaximumLength(255)
+            .WithMessage(ValidationMessage.MaxLength255Message)
+            .MustAsync(BeUniqueName)
+            .WithMessage(ValidationMessage.UniqueMessage)
+            .WithErrorCode("Unique");
+
+        RuleFor(v => v.Type)
+            .NotEmpty()
+            .WithMessage(ValidationMessage.RequiredMessage)
+            .Must(t => t == null || PokemonType.SupportedTypes.Any(st => st.Name == t))
+            .WithMessage(ValidationMessage.UnsupportedTypeMessage);
+        
+        RuleFor(v => v.Power)
+            .GreaterThan(0)
+            .WithMessage(ValidationMessage.PositiveMessage)
+            .LessThanOrEqualTo(255)
+            .WithMessage(ValidationMessage.MaxValue255Message);
+        
+        RuleFor(v => v.Accuracy)
+            .GreaterThan(0)
+            .WithMessage(ValidationMessage.PositiveMessage)
+            .LessThanOrEqualTo(255)
+            .WithMessage(ValidationMessage.MaxValue255Message);
+        
+        RuleFor(v => v.Pp)
+            .GreaterThan(0)
+            .WithMessage(ValidationMessage.PositiveMessage)
+            .LessThanOrEqualTo(255)
+            .WithMessage(ValidationMessage.MaxValue255Message);
+    }
+    
+    public async Task<bool> BeUniqueName(CreateMoveCommand model, string name,
+        CancellationToken cancellationToken)
+    {
+        return !await _context.Moves
+            .AnyAsync(mv => mv.Name == name, cancellationToken);
+    }
+}
