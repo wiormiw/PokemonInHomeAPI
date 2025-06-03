e50b05bba8fe1aa4582b5561c8c7691e2f9bf9e6
Wijoyo Raharjo Murti Indra Wahyono <wwijoyo2@gmail.com>
2025-06-03 12:15:41 +0700

    feat: Pagination validation message

--- src/Domain/Constants/ValidationMessage.cs
+++ src/Domain/Constants/ValidationMessage.cs
@@ -8,5 +8,8 @@ public static class ValidationMessage
     public const string PositiveMessage = "'{PropertyName}' must be positive.";
     public const string MaxValue255Message = "'{PropertyName}' must be less than or equal to 255.";
     public const string UnsupportedTypeMessage = "'{PropertyName}' type not supported.";
+    
+    // Pagination
+    public const string MinValue1Message = "'{PropertyName}' must be greater than or equal to 1.";
 }
 
