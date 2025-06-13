91fbdc017fefa803e99e86643742aa4237f91b4f
Wijoyo Raharjo Murti Indra Wahyono <wwijoyo2@gmail.com>
2025-06-13 16:10:54 +0700

    feat: Add CatchPokemon method in Player Entity

--- src/Domain/Entities/Player.cs
+++ src/Domain/Entities/Player.cs
@@ -3,7 +3,7 @@
 public class Player : BaseAuditableEntity
 {
     public string ApplicationUserId { get; set; } = string.Empty;
-    
+
     public ICollection<PlayerPokemon> PlayerPokemons { get; set; } = new List<PlayerPokemon>();
     public ICollection<Pokedex> Pokedexes { get; set; } = new List<Pokedex>();
     public ICollection<PlayerItem> PlayerItems { get; set; } = new List<PlayerItem>();
@@ -11,4 +11,22 @@ public class Player : BaseAuditableEntity
     public ICollection<Battle> Player2Battles { get; set; } = new List<Battle>();
     public ICollection<Trade> Player1Trades { get; set; } = new List<Trade>();
     public ICollection<Trade> Player2Trades { get; set; } = new List<Trade>();
+
+    public PlayerPokemon CatchPokemon(PokemonSpecies species, Pokemon wildPokemon, string nickname)
+    {
+        var playerPokemon = new PlayerPokemon
+        {
+            PlayerId = this.Id,
+            Pokemon = wildPokemon,
+            Nickname = nickname,
+            CaughtAt = DateTimeOffset.UtcNow,
+            IsActive = false,
+        };
+
+        this.PlayerPokemons.Add(playerPokemon);
+
+        this.AddDomainEvent(new PokemonCaughtEvent(species.Id, this.Id));
+
+        return playerPokemon;
+    }
 }
