76e8af0972b204a21972701376b3108bf6a0a002
Wijoyo Raharjo Murti Indra Wahyono <wwijoyo2@gmail.com>
2025-06-13 16:09:08 +0700

    feat: Add new operation to update the pokedex data when creating pokemon

--- src/Application/Pokemons/EventHandlers/PokemonCreatedEventHandler.cs
+++ src/Application/Pokemons/EventHandlers/PokemonCreatedEventHandler.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.Extensions.Logging;
+using PokemonInHomeAPI.Application.Common.Interfaces;
+using PokemonInHomeAPI.Domain.Entities;
 using PokemonInHomeAPI.Domain.Events;
 
 namespace PokemonInHomeAPI.Application.Pokemons.EventHandlers;
@@ -6,16 +8,37 @@ namespace PokemonInHomeAPI.Application.Pokemons.EventHandlers;
 public class PokemonCreatedEventHandler : INotificationHandler<PokemonCreatedEvent>
 {
     private readonly ILogger<PokemonCreatedEventHandler> _logger;
+    private readonly IApplicationDbContext _context;
 
-    public PokemonCreatedEventHandler(ILogger<PokemonCreatedEventHandler> logger)
+    public PokemonCreatedEventHandler(ILogger<PokemonCreatedEventHandler> logger, IApplicationDbContext context)
     {
         _logger = logger;
+        _context = context;
     }
 
-    public Task Handle(PokemonCreatedEvent notification, CancellationToken cancellationToken)
+    public async Task Handle(PokemonCreatedEvent notification, CancellationToken cancellationToken)
     {
         _logger.LogInformation("PokemonInHomeAPI Domain Event: {DomainEvent}", notification.GetType().Name);
 
-        return Task.CompletedTask;
+        var allPlayerIds = await _context.Players
+            .Select(p => p.Id)
+            .ToListAsync(cancellationToken);
+
+        foreach (var playerId in allPlayerIds)
+        {
+            var pokedexEntryExists = await _context.Pokedexes
+                .AnyAsync(p => p.PlayerId == playerId && p.SpeciesId == notification.Pokemon.Id, cancellationToken); // Pokemon.Id refer to PokemonSpecies that created.
+
+            if (!pokedexEntryExists)
+            {
+                _context.Pokedexes.Add(new Pokedex
+                {
+                    PlayerId = playerId,
+                    SpeciesId = notification.Pokemon.Id,
+                    Seen = false,
+                    Caught = false
+                });
+            }
+        }
     }
 }
