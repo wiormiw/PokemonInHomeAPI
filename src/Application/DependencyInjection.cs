b8b91568159ed4d8ca5c6b1bf5fa0999e7a78627
Wijoyo Raharjo Murti Indra Wahyono <wwijoyo2@gmail.com>
2025-06-16 11:41:19 +0700

    fix: Fix typo in dependency injection application proj

--- src/Application/DependencyInjection.cs
+++ src/Application/DependencyInjection.cs
@@ -19,7 +19,7 @@ public static class DependencyInjection
             cfg.AddBehavior(typeof(IPipelineBehavior<,>), typeof(AuthorizationBehaviour<,>));
             cfg.AddBehavior(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
             cfg.AddBehavior(typeof(IPipelineBehavior<,>), typeof(PerformanceBehaviour<,>));
-            cfg.AddBehavior(typeof(TransactionBehaviour<,>), typeof(TransactionBehaviour<,>));
+            cfg.AddBehavior(typeof(IPipelineBehavior<,>), typeof(TransactionBehaviour<,>));
         });
     }
 }
